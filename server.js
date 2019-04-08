b9118867845d9f9ab9fac48a6a19ae5988cc2a36
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-04-07 22:21:38 -0400

    uncommented index.html and server.js

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-// var express = require("express");
-// var logger = require("morgain");
-// var mongoose = require("mongoose");
+var express = require("express");
+var logger = require("morgain");
+var mongoose = require("mongoose");
 
-// // Below is the npm packages we use to scrape the website for articles
-// var axios = require("axios");
-// var cheerio = require("cheerio");
+// Below is the npm packages we use to scrape the website for articles
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-// var db = require("./models");
+var db = require("./models");
 
-// var PORT = 3100;
+var PORT = 3100;
 
-// var app = express();
+var app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// //making public folder a static folder
-// app.use(express.static("public"));
+//making public folder a static folder
+app.use(express.static("public"));
 
-// // Connecting to Mongo Data Base
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/heroku_smtnsm76";
+// Connecting to Mongo Data Base
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/heroku_smtnsm76";
 
-// mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
-// // below are all the routes for the web app
-
-
-// // this is a GET route. It is used to scrape the wikipedia website
-// app.get("/scrape", function(req,res) {
-//     // grab the html body
-//     axios.get("https://en.wikipedia.org/wiki/Main_Page").then(function(response) {
-//         // this is to load the site into cheerio and save it
-//         var $ = cheerio.load(response.data);
+// below are all the routes for the web app
 
 
+// this is a GET route. It is used to scrape the wikipedia website
+app.get("/scrape", function(req,res) {
+    // grab the html body
+    axios.get("https://en.wikipedia.org/wiki/Main_Page").then(function(response) {
+        // this is to load the site into cheerio and save it
+        var $ = cheerio.load(response.data);
 
-//     })
-// })
+        
+
+    })
+})
