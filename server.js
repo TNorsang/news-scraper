85a8e23c40b32ee9be7a4da4adb9f15bed0792c2
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-04-09 00:15:51 -0400

    added stuff

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+// Express/Logger/Mongoose Dependencies
 var express = require("express");
-var logger = require("morgain");
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Below is the npm packages we use to scrape the website for articles
@@ -8,25 +9,27 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 3100;
+// Setting up the port
+var PORT = 3000;
 
 var app = express();
 
+// Logging requests via Morgan
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//making public folder a static folder
+// Making public folder a static folder
 app.use(express.static("public"));
 
 // Connecting to Mongo Data Base
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/heroku_smtnsm76";
+var MONGODB_URI =  "mongodb://localhost/heroku_smtnsm76";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
 
-// below are all the routes for the web app
 
+// Below are all the routes for the web app
 
 // this is a GET route. It is used to scrape the wikipedia website
 app.get("/scrape", function(req,res) {
@@ -35,7 +38,31 @@ app.get("/scrape", function(req,res) {
         // this is to load the site into cheerio and save it
         var $ = cheerio.load(response.data);
 
-        
+        $("article h2").each(function(i, element) {
+            
+            var result = {};
 
-    })
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        });
+        
+        res.send("Scrape Complete");
+    
+    });
 })
+
+app.listen(PORT, function() {
+    console.log("Hello User! This app is running on " + PORT + "!")
+});
